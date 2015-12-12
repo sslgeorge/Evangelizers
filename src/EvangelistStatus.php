bb82820acdfe25a3046f2c4e2328a00e154a5735
George James <george.james@andela.com>
2015-12-12 15:06:28 -0500

    Applied fixes from StyleCI

--- src/EvangelistStatus.php
+++ src/EvangelistStatus.php
@@ -2,13 +2,11 @@
 
 namespace C3P0\App;
 
-use GithubClient;
-
 class EvangelistStatus
 {
     protected $username;
     protected $api_client;
-    
+
     public function __construct($username, $api_client)
     {
         $this->username = $username;
